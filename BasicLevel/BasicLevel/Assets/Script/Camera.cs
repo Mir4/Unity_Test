a1118149b3eaaf05cced725f6c4ed4ca52918b7a
Mir4 <32401615+Mir4@users.noreply.github.com>
2018-03-11 17:04:06 +0300

    Base

---
+++ BasicLevel/BasicLevel/Assets/Script/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject Sphere;
+    private Vector3 distanse;
+    void Start()
+    {
+        distanse = transform.position - Sphere.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Sphere.transform.position + distanse;
+    }
+}
